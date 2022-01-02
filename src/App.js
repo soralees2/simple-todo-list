887e7a45965333a0d16e8883efc65a878d3c5e19
eelsoras2 <eelsoras2@gmail.com>
2022-01-02 22:48:53 +0900

    simple todolist example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [toDo, setToDo] = useState("");
+	const [toDos, setToDos] = useState([]);
+	const onChange = (event) => setToDo(event.target.value);
+	const onSubmit = (event) => {
+		event.preventDefault();
+		if(toDo === ""){ // input 값이 공백일 경우 함수 실행시키지 않음
+			return;
+		}
+		setToDos((currentArray) => [toDo, ...currentArray]);
+		setToDo(""); // submit 이벤트 작동시 toDo의 state값을 ""으로 셋텡
+		console.log(toDos);
+	}
+
+	return (
+		<div className="App">
+			<h1>My Todos ({toDos.length})</h1>
+			<form onSubmit={onSubmit}>
+				<input type="text" onChange={onChange} placeholder="Write your to do..."/>
+				<button>Add To Do</button>
+			</form>
+		</div>
+	);
 }
 
 export default App;
